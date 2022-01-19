137360b0d46ecefb1eb6d68e1b3f3ec579efb575
karen1998 <karen.yeghiazaryan@inomma.com>
2022-01-19 14:32:15 +0400

    add minimal number check && code refactor

--- min_max.sh
+++ min_max.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 number_regex="^[a-zA-Z_]"
-arg1=$1
-arg2=$2
-arg3=$3
 
 
 # Check for valid count of arguments
@@ -18,13 +15,19 @@ if [[ "$arg1" =~ $number_regex || "$arg2" =~ $number_regex || "$arg3" =~ $number
   exit 1
 fi
 
-# Check for maximal number
-if (($arg1 > $arg2 && $arg1 > $arg3)); then
-	echo ${arg1}
-elif (($arg2 > $arg1 && $arg2 > $arg3)); then
-	echo $arg2
-else
-	echo $arg3
-fi
+max=$1 # First command line argumner
+min=$2 # Second command line argumner
+
+# Check for min and max numebrs
+for number in "$@" 
+do
+	if (($number > max)); then
+		max=$number
+	elif (($number < min)); then
+		min=$number
+	fi
+done
+
+echo "Min number is ${min}, and max number is ${max}"
 
 exit 0
