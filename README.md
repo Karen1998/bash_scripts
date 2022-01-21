68ad10f8c46d31afd831392e2a267dbf5ee676b4
karen1998 <karen.yeghiazaryan@inomma.com>
2022-01-21 22:17:15 +0400

    factorial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bash_scripts
-Script will return minimal and maximal numbers given exact 3 numbers
+
+min_max.sh -> Script will return minimal and maximal numbers given exact 3 numbers
+factorial -> Will return factorial for given number
---
+++ factorial.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+number_regex="^[a-zA-Z_]"
+
+# Check for validity of arguments
+if [[ "$1" =~ $number_regex ]]; then
+    echo "Arguments must be numbers only."
+    exit 1
+fi
+
+if [[ "$#" < 1 ]]; then
+    echo "Enter number"
+    exit 1
+fi
+
+# Validate inputed number
+message=''
+number=$1
+
+if [[ "$#" > 1 ]]; then
+    message="Script had take only fisrt argumnet (${number})"
+fi
+
+# Create function
+factorial_func() {
+    if [[ $1 == 1 ]]; then
+        echo 1
+    else
+        factorial_sum=$(factorial_func $(($1 - 1)))
+        echo $(($1 * factorial_sum))
+    fi
+}
+
+let factorial="$(factorial_func $number)"
+
+echo -e "${factorial} \n${message}"
+exit 0
